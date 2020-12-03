770327e5355b62073066dbc65f63784eedf3b4de
cheshi-mantu <cheshi.mantu@pm.me>
2020-12-03 17:37:52 +0300

    updated gradle and removed default remote driver URL

--- src/test/java/helpers/Environment.java
+++ src/test/java/helpers/Environment.java
@@ -3,7 +3,7 @@ package helpers;
 public class Environment {
     public static final String
         demoqaUrl = System.getProperty("url","https://demoqa.com"),
-        remoteDriverUrl = System.getProperty("remote_driver_url","http://192.168.1.123:4444/wd/hub/"),
+        remoteDriverUrl = System.getProperty("remote_driver_url"), //"http://192.168.1.123:4444/wd/hub/"
         videoStorageUrl = System.getProperty("video_storage_url");
     public static boolean
         isRemoteDriver = remoteDriverUrl != null,
