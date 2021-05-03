b5a7943b139e4946c9808daea01ad568a350c132
Egor Ivanov <egor.ivanov@pm.me>
2021-05-03 13:47:09 +0300

    allure-ee-junit-platform an be removed as selective tests run moved to opensource

--- src/test/java/tests/DemoQaAlerts.java
+++ src/test/java/tests/DemoQaAlerts.java
@@ -1,9 +1,6 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -17,12 +14,10 @@ import static io.qameta.allure.Allure.step;
 @Tag("alerts_tests")
 class DemoQaAlerts extends TestBase {
     @Test
-    @DisplayName("Alerts tests")
+    @AllureId("3")
+    @DisplayName("integration with pycharm")
     @Description("Open Alerts page, make tests for alerts processing")
     void fromTest() {
-        step ("Open froms page", () -> {
-            open("https://demoqa.com/alertsWindows");
-        });
     }
 
 }
