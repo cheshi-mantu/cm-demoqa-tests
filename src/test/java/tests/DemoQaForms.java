ad062bef60da8797525036aa4d5d25b2e65b1454
egorivanov <egor.ivanov@pm.me>
2021-06-14 09:05:50 +0300

    updated build.gradle added filling of name, surname, email address

--- src/test/java/tests/DemoQaForms.java
+++ src/test/java/tests/DemoQaForms.java
@@ -1,31 +1,64 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import utils.RandomUtils;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static helpers.AttachmentsHelper.attachScreenshot;
 import static helpers.Environment.demoqaUrl;
+import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
 
-@Epic("Demo QA - demoqa.com")
-@Feature("Demo QA - demoqa.com :: Forms")
-@Story("Automated tests for Forms")
-@Tag("forms_tests")
+@Epic("demoqa.com tests")
+@Feature("Elements")
+@Owner("egorivanov")
+@Tag("form_tests")
+
 class DemoQaForms extends TestBase {
+
+    String firstName = "";
+    String lastName = "";
+    String email = "";
+    String address = "";
+    String addressExt = "";
+
     @Test
     @DisplayName("Forms tests")
     @Description("Open forms page, make tests for filling forms and checks")
-    void fromTest() {
-        step ("Open froms page", () -> {
+    void formTest() {
+        step ("Open form training page", () -> {
             open(demoqaUrl + "/automation-practice-form");
-            attachScreenshot("Forms practice first screen");
+            parameter("Page URL", demoqaUrl + "/automation-practice-form");
         });
+        step ("Fill name and email fields", () -> {
+            firstName = RandomUtils.getRandomString(7);
+            lastName = RandomUtils.getRandomString(10);
+            email = RandomUtils.getRandomEmail();
+
+            Allure.addAttachment("First Name", firstName);
+            Allure.addAttachment("Last Name", lastName);
+            Allure.addAttachment("Email address", email);
+
+            $("#firstName").val(firstName);
+            $(":focus").pressTab();
+            $(":focus").val(lastName);
+            $(":focus").pressTab();
+            $("#userEmail").val(email);
+        });
+//        step ("Fill name fields", () -> {
+//            firstName = RandomUtils.getRandomString(7);
+//            lastName = RandomUtils.getRandomString(10);
+//            Allure.addAttachment("First Name", firstName);
+//            Allure.addAttachment("Last Name", lastName);
+//            $("#firstName").val(firstName);
+//            $(":focus").pressTab();
+//            $(":focus").val(lastName);
+//        });
+
     }
 
 }
